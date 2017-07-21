2afd7689fd4d94746f52178a480c3de396bd2c59
Sauvik Dolui <sauvikdolui@Sauviks-MacBook-Air.local>
2017-07-22 02:54:59 +0530

    Initial Push

---
+++ SwiftLint/TestFile.swift
@@ -0,0 +1,28 @@
+//
+//  TestFile.swift
+//  SwiftLint
+//
+//  Created by Sauvik Dolui on 22/07/17.
+//  Copyright © 2017 Sauvik Dolui. All rights reserved.
+//
+
+import Foundation
+
+// Working with Swift Protocols
+struct Student: Equatable {
+    let name: String
+    let rollNumber: Int
+}
+
+extension Student: CustomDebugStringConvertible {
+    var debugDescription: String {
+        return "\(type(of:self)): {name: \(name), rollNumber: \(rollNumber)}"
+    }
+}
+
+extension Student: CustomStringConvertible {
+    var description: String {
+        return "\(type(of:self)): {name: \(name), rollNumber: \(rollNumber)}"
+    }
+}
+func == (lhs: Student, rhs: Student) -> Bool { return lhs.name == rhs.name && lhs.rollNumber == rhs.rollNumber }
--- SwiftLint/ViewController.swift
+++ SwiftLint/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+
+enum CharacterTypes {
+    case ninja
+    case pirate
+}
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -18,8 +23,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // TODO:
+        let msg = "hey ther   ,  or2"
+        print(msg)
     }
-
 
 }
 
+extension ViewController {
+
+}
